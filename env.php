80bee901af37b0b33d8a24430ece138528735966
Ali Jose Mata Grimont <62219137+alijmg20@users.noreply.github.com>
2021-06-29 00:25:37 -0400

    remote hosting

--- env.php
+++ env.php
@@ -1,8 +1,14 @@
 <?php
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'projectnetworks';
+// $server = 'localhost';
+// $user = 'root';
+// $password = '';
+// $database = 'projectnetworks';
+
+$server = 'bfpseav2bexfmn52r38v-mysql.services.clever-cloud.com';
+$database = 'bfpseav2bexfmn52r38v';
+$user = 'uphzzjergkscagjw';
+$password = 'uphzzjergkscagjw';
+
 
 $connection = new mysqli($server,$user,$password,$database);
 
